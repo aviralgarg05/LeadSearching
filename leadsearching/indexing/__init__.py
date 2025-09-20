40922594758fe7dc0e1e1b345855ba1076518648
aviralgarg05 <gargaviral99@gmail.com>
2025-09-20 23:13:50 +0530

    chore: clean up codacy config and unused indexing __init__\n\n- remove .codacy/codacy.yaml\n- remove redundant leadsearching/indexing/__init__.py

--- leadsearching/indexing/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Indexing subpackage for LeadSearching.
-
-This module groups vector index build functionality.
-"""
-
-# Re-export build helper for convenience
-from .build_index import build_index  # noqa: F401
